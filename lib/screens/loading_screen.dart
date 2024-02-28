bfc3a7cd0289f47ebf199b4fb9a38fc04b9b9924
Mohammad Salim <mhmadip@gmail.com>
2024-02-29 01:31:57 +0300

    add getData function besides the http package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima1_oop2/services/location.dart';
+import 'package:clima1_oop2/services/networking.dart';
+
+const apiKey='8137ace68448d41da44e9bf9fd681a85';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,14 +13,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+late String url;
 
   void getLocation()async{
     determinePosition() ;
     GeolocatorPlatform location = GeolocatorPlatform.instance;
     final position= await location.getCurrentPosition();
-
-    print(position.latitude);
-    print(position.longitude);
+    double long= position.longitude;
+    double lat=position.latitude;
+    url='https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apiKey';
+    NetworkHelper networkHelper= NetworkHelper(url);
+    networkHelper.getData();
 
   }
 
@@ -30,6 +36,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
           onPressed: () {
             //Get the current location
             getLocation();
+
           },
           child: const Text('Get Location'),
         ),
@@ -37,3 +44,4 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
